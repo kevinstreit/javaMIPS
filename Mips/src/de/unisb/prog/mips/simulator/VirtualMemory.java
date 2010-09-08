adfe1f4fd5db141061d80575383148297c1a88d6
Sebastian Hack <hack@cs.uni-sb.de>
2010-09-08 08:29:28 +0200

    Completed draft of virtual memory

--- Mips/src/de/unisb/prog/mips/simulator/VirtualMemory.java
+++ Mips/src/de/unisb/prog/mips/simulator/VirtualMemory.java
@@ -3,24 +3,50 @@ package de.unisb.prog.mips.simulator;
 public class VirtualMemory implements Memory {
 	
 	// Page size in ints
-	public static int PAGE_SIZE = 1024;
+	
+	private static final int[] OFFSETS = { 32, 22, 12 };
+	private static final int LAST      = OFFSETS[OFFSETS.length - 1];
+	public static final int PAGE_SIZE  = 1 << (LAST - 2);
 	
 	private int[][] pages;
+	private PageFaultHandler handler;
 	
 	public VirtualMemory(int nPages) {
 		this.pages = new int[nPages][];
+		this.pages[0] = new int[PAGE_SIZE];
+	}
+	
+	private int[] findPage(int addr, int[] page, int level) {
+		if (level >= OFFSETS.length)
+			return page;
+		int ofs  = OFFSETS[level];
+		int mask = (1 << OFFSETS[level - 1]) - (1 << ofs);
+		int idx  = (mask & addr) >> ofs;
+		int paddr = page[idx];
+		if (paddr == 0)
+			handler.handle();
+		return findPage(addr, pages[paddr], level + 1);
+	}
+	
+	private int[] lookupPage(int addr) {
+		return findPage(addr, pages[0], 1);
+	}
+	
+	private static int getOffset(int addr) {
+		return addr & ((1 << LAST) - 1);
 	}
 	
 	@Override
 	public int load(int addr) {
-		// TODO Auto-generated method stub
-		return 0;
+		int[] page = lookupPage(addr);
+		int offset = getOffset(addr);
+		return page[offset];
 	}
 
 	@Override
 	public void store(int addr, int word) {
-		// TODO Auto-generated method stub
-
+		int[] page = lookupPage(addr);
+		int offset = getOffset(addr);
+		page[offset] = word;
 	}
-
 }
