884b8646974a44b2d70f787583caed82db23a96c
hack <hack@banane.cs.uni-saarland.de>
2010-09-08 11:56:21 +0200

    Added Types and easy memory

---
+++ Mips/src/de/unisb/prog/mips/simulator/CoarseMemory.java
@@ -0,0 +1,35 @@
+package de.unisb.prog.mips.simulator;
+
+public class CoarseMemory {
+	
+	private final Memory mem;
+	boolean bigEndian;
+	
+	public CoarseMemory(Memory mem) {
+		this.mem = mem;
+	}
+
+	public void store(int addr, int val, Type tp) {
+		int sz    = tp.sizeof();
+		int start = bigEndian ? 0 : (sz - 1);
+		int inc   = bigEndian ? 1 : -1;
+		for (int i = addr + start; i < sz; i += inc) {
+			byte b = (byte) (val & 0xff);
+			val >>>= 8;
+			mem.store(addr, b);
+		}
+	}
+
+	public int load(int addr, Type tp) {
+		int res   = 0;
+		int sz    = tp.sizeof();
+		int start = bigEndian ? 0 : (sz - 1);
+		int inc   = bigEndian ? 1 : -1;
+		for (int i = addr + start; i < sz; i += inc) {
+			res |= mem.load(i) & 0xff;
+			res <<= 8;
+		}
+		return res;
+	}
+
+}
