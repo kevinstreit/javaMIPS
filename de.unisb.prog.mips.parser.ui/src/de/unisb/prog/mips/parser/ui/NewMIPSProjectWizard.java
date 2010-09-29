ed2779c0abc87fd3d634cd8ce4cbd7f02cd4ff61
Kevin Streit <streit@cs.uni-saarland.de>
2010-09-29 11:16:43 +0200

    Added new project wizard

---
+++ de.unisb.prog.mips.parser.ui/src/de/unisb/prog/mips/parser/ui/NewMIPSProjectWizard.java
@@ -0,0 +1,65 @@
+package de.unisb.prog.mips.parser.ui;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+import org.eclipse.xtext.ui.XtextProjectHelper;
+
+public class NewMIPSProjectWizard extends Wizard implements INewWizard {
+	
+	private WizardNewProjectCreationPage pageOne;
+
+	public NewMIPSProjectWizard() {
+		setWindowTitle("New MIPS Project");
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		
+	}
+
+	@Override
+	public boolean performFinish() {
+		IProject newProject = pageOne.getProjectHandle();
+		
+		try {
+			newProject.create(null);
+			newProject.open(null);
+		} catch (CoreException ex) {
+			pageOne.setErrorMessage("Could not create the new project.");
+			ex.printStackTrace();
+			return false;
+		}
+		
+		try {
+			IProjectDescription projectDesc = newProject.getDescription();
+			projectDesc.setNatureIds(new String[] {
+					XtextProjectHelper.NATURE_ID
+				});
+			newProject.setDescription(projectDesc, null);
+		} catch (CoreException ex) {
+			pageOne.setErrorMessage("Could not add the MIPS Nature to the project.");
+			ex.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public void addPages() {
+		super.addPages();
+		
+		pageOne = new WizardNewProjectCreationPage("New MIPS Project");
+		pageOne.setTitle("MIPS Project");
+		pageOne.setDescription("Create a new MIPS project.");
+		
+		addPage(pageOne);
+	}
+
+}
