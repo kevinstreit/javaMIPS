c93b4566837036c6d8d2737920ea6bc3cda139d1
Kevin Streit <streit@cs.uni-saarland.de>
2010-10-25 16:27:17 +0200

    Made MIPS file default content contain main label again; Made default content to be used only for the first MIPS file in each project

--- de.unisb.prog.mips.parser.ui/src/de/unisb/prog/mips/parser/ui/wizards/NewMIPSFileWizard.java
+++ de.unisb.prog.mips.parser.ui/src/de/unisb/prog/mips/parser/ui/wizards/NewMIPSFileWizard.java
@@ -3,6 +3,11 @@ package de.unisb.prog.mips.parser.ui.wizards;
 import java.io.InputStream;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
@@ -22,7 +27,32 @@ public class NewMIPSFileWizard extends Wizard implements INewWizard {
 
 		@Override
 		protected InputStream getInitialContents() {
-			return NewMIPSFileWizard.class.getResourceAsStream("/config/defaultFile.mips");
+			final IPath containerPath = getContainerFullPath();
+			IPath newFilePath = containerPath.append(getFileName());
+			final IFile newFileHandle = createFileHandle(newFilePath);
+
+			IProject proj = newFileHandle.getProject();
+
+			if (proj != null && proj.exists() && proj.isOpen()) {
+				final boolean[] containsMIPSFile = new boolean[]{ false };
+
+				try {
+					proj.accept(new IResourceVisitor() {
+						public boolean visit(IResource resource) throws CoreException {
+							if (resource instanceof IFile && resource.getFileExtension().equals("mips"))
+								containsMIPSFile[0] = true;
+
+							return !containsMIPSFile[0];
+						}
+					});
+				} catch (CoreException e) {
+					containsMIPSFile[0] = false;
+				}
+
+				return containsMIPSFile[0] ? null : NewMIPSFileWizard.class.getResourceAsStream("/config/defaultFile.mips");
+			}
+
+			return null;
 		}
 	}
 
